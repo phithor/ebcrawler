efc3c4ffd91b9e8faf5e56a69e3cfdccc1036c54
Mathias Thorsen <6831666+phithor@users.noreply.github.com>
2018-07-22 12:06:04 +0200

    Added function parse_results. Cleaned Project

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
-flysasApp
+ebcrawler
 =========
 
-This is a simple tool to check prices and availability for SAS flights. Work in progress. Inspired by https://github.com/travelhacks/flysasClient
-
+This is a simple tool to download your EB history from the SAS website, either for
+viewing or in a format suitable for example to import into Excel.
 
 Requirements
 ------------
 
 * Python3
 * requests library (can usually be installed with `pip install requests`)
+* Pandas
 
-Everything else should be included in python3 by default.
+Usage
+------------
 
+With the sasCrawler.py file or Jupyter Notebook, get results by calling function fetch_prices
+
+a = fetch_prices(From, To, OutDate, Indate, Type)
+
+From, To - IATA Airport Code
+OutDate, Indate - Format YYYYMMDD
+Type - "star"(Star Alliance award flights), "revenue" (Regular revenue fares) or "points" (SAS awards flights)
+
+The output of the fetch_prices function is for now in json. 
+
+To parse the prices in to a Pandas DataFrame, use the function parse_results:
+
+Example in Jupyter notebook:
+
+from sasCrawler import fetch_price, parse_results
+
+a = fetch_price("OSL", "HKG", "20190301", "20190308", "star")
+df_out, df_in = parse_results(a)
+
+
+The flights are now in their respective DataFrame table. 
+=======
--- sasCrawler.py
+++ sasCrawler.py
@@ -6,9 +6,11 @@ import argparse
 import getpass
 import csv
 from datetime import datetime
+import pandas as pd
 
 
 def getAccessToken(eb, pw):
+    ''' Get access token to poll api '''
     r = requests.post('https://api.flysas.com/authorize/oauth/token',
                       data={'grant_type': 'password', 'username': eb, 'password': pw},
                       headers={'Referer': 'https://www.sas.no/',
@@ -18,10 +20,17 @@ def getAccessToken(eb, pw):
                                'accept': 'application/json, text/plain, */*'
                       },
                       )
+    if r.status_code != 200:
+        print("Failed to log in:")
+        print(r.text)
+        sys.exit(1)
+
     rjson = r.json()
     return rjson
 
+
 def fetch_page(pageno, tokenjson):
+    ''' Fetch page of EB history '''
     print("Fetching page {0}".format(pageno))
     token = tokenjson['access_token']
     sessionid = tokenjson['customerSessionId']
@@ -39,38 +48,43 @@ def fetch_page(pageno, tokenjson):
 
     return r.json()['euroBonus']
 
-def fetch_price(From, To, Return=True, outDate, inDate, Award):
-    ''' Fetch prices from SAS API
+def fetch_price(From, To, outDate, inDate, booking_type, tokenjson="not"):
+    ''' Fetch prices from SAS API:
 
-        fetch_price(From, To, Return=True, outDate, inDate, Award)
+        fetch_price(From, To, outDate, inDate, booking_type)
         
         example url: (parameteres in new line for readability)
         
         https://api.flysas.com/offers/flights?
-        to=AGP&
-        from=OSL&
-        outDate=20180715&
-        inDate=20180729&
+        to="Airport Code"&
+        from="Airport Code"&
+        outDate=20180715& #YYYYMMDD
+        inDate=20180729& #YYYYMMDD
         adt=2&
         chd=0&
         inf=0&
         yth=0&
-        bookingFlow=revenue&
+        bookingFlow="points" / "revenue" /"star"&
         pos=no&
         channel=web&
         displayType=upsell
     '''
 
-    baseurl = 'https://api.flysas.com/offers/flights?'
-    url = baseurl + 'to={0}&from={1}&outDate={2}&inDate={3}&adt={4}&chd=0&inf=0&yth=0&bookingFlow=revenue&pos=no&channel=web&displayType=upsell
-
     
+    
+    baseurl = 'https://api.flysas.com/offers/flights?'
+    url = baseurl + 'to={0}&from={1}&outDate={2}&inDate={3}&adt={4}&chd=0&inf=0&yth=0&bookingFlow={5}&pos=no&channel=web&displayType=upsell'.format(To, From, outDate, inDate, "2" ,booking_type)
 
-    r = requests.get(
+    if tokenjson != "not":  #Not needed to get an offer
+        token = tokenjson['access_token'] 
+        sessionid = tokenjson['customerSessionId'] 
+
+    r = requests.get(url)
+    rjson = r.json()
+    return rjson
 
 
-
-destination_lists = [('CPH','NRT'),
+eb_destination_list = [('CPH','NRT'),
                      ('ARN','EWR'),
                      ('ARN','HKG'),
                      ('CPH','HKG'),
@@ -88,3 +102,82 @@ destination_lists = [('CPH','NRT'),
                      ('OSL','MIA')]
                      
 
+# Works
+#a = fetch_price("OSL", "SIN", "20181101", "20181108", "star")
+
+def parse_results(results):
+
+    ### Divide reulsts dict in the two halves
+    outflights = results["outboundFlights"]
+    inflights = results["inboundFlights"]
+
+    print("Number of results Out: " +str(len(outflights)))
+    print("Number of results In: " +str(len(inflights)))
+
+    ### Define parse functions that are not lambdable
+
+    def parse_via(x):
+        stop_str = ''
+        counter = 0
+        for i in range(len(x)):
+            if counter > 0:
+                stop_str += ','
+            counter += 1
+            stop_str += x[i]['code']
+            
+        return stop_str
+
+    def parse_fares(x):
+        fare_keys = list(x.keys())
+        avl_seats = ""
+        counter = 0
+        for i in range(len(x)):
+            if counter > 0:
+                avl_seats += ','
+            avl_seats += str(x[fare_keys[i]]['avlSeats'])
+            counter += 1
+        return avl_seats
+
+    def parse_prices(x):
+        fare_keys = list(x.keys())
+        prices = ""
+        counter = 0
+        for i in range(len(x)):
+            if counter > 0:
+                prices += ','
+            prices += str(int(x[fare_keys[i]]['points']))
+            counter += 1
+        return prices
+
+    def to_df(flights):
+        #df3 = df_in.copy()
+
+        df_flights = pd.DataFrame.from_dict({(i): flights[i] 
+                               for i in flights.keys()},
+                           orient='index')
+
+        df_new = pd.DataFrame(index=df_flights.index.copy())
+        df_new['origin'] = df_flights.origin.apply(lambda x: x["code"])
+        df_new['destination'] = df_flights.destination.apply(lambda x: x["code"])
+        df_new['Departure Date'] = df_flights.startTimeInLocal.apply(lambda x: x.split('T')[0])
+        df_new['Departure Time'] = df_flights.startTimeInLocal.apply(lambda x: x.split('T')[1].split('.')[0])
+        df_new['Arrival Date'] = df_flights.endTimeInLocal.apply(lambda x: x.split('T')[0])
+        df_new['Arrival Time'] = df_flights.endTimeInLocal.apply(lambda x: x.split('T')[1].split('.')[0])
+        df_new['connectionDuration'] = df_flights['connectionDuration']
+        df_new['via'] = df_flights.via.apply(parse_via)
+        df_new['Cabin Classes'] = df_flights.cabins.apply(lambda x: list(x.keys()) if x != 'NaN' else '-NA-')
+        df_new['avlSeats'] = df_flights.lowestFares.apply(parse_fares)
+        df_new['Points'] = df_flights.lowestFares.apply(parse_prices)
+        df_new.head()
+
+        return df_new
+
+    df_outflights = to_df(outflights)
+    df_inflights = to_df(inflights)
+
+    return df_outflights, df_inflights
+
+
+
+#def scan_availability(From, To, FromDate, ToDate):
+#    
